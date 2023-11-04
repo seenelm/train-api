3828d8c28795da2ef4906c4be298ccc1d6e5a71a
noahgross1 <noahgross1@gmail.com>
2023-11-04 15:29:34 -0400

    TRAIN-24: added updateUsersFullName

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import UserService from "../services/UserService";
 
+const userService = new UserService();
+
 // Find user's groups.
 export const fetchGroups = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result = await UserService.fetchGroups(req.params.userId);
+    const result = await userService.fetchGroups(req.params.userId);
     return res.status(201).json(result.userGroups);
   } catch (error) {
     next(error);
@@ -15,18 +17,36 @@ export const fetchGroups = async (req: Request, res: Response, next: NextFunctio
 export const findUsers = async (req: Request, res: Response, next: NextFunction) => {
   const { search } = req.query;
   try {
-    const users = await UserService.findUsers(search);
+    const users = await userService.findUsers(search);
     return res.status(201).json(users);
   } catch (error) {
     next(error);
   }
 };
 
+/**
+ * Create or update users bio.
+ * @param req 
+ * @param res 
+ * @param next 
+ * @returns 
+ */
 export const updateUserBio = async (req: Request, res: Response, next: NextFunction) => {
   const { userBio } = req.body;
   const { userId } = req.params;
   try {
-    await UserService.updateUserBio(userId, userBio);
+    await userService.updateUserBio(userId, userBio);
+    return res.status(201).json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const updateUsersFullName = async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body;
+  const { userId } = req.params;
+  try {
+    await userService.updateUsersFullName(userId, name);
     return res.status(201).json({ success: true });
   } catch (error) {
     next(error);
