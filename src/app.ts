709772fc5660be6cbb431693ac976b7023ab97b3
noahgross1 <noahgross1@gmail.com>
2025-03-08 18:50:09 -0500

    added cloud storage

--- src/app.ts
+++ src/app.ts
@@ -14,11 +14,26 @@ import searchRouter from "./route/searchRouter";
 import eventRouter from "./route/eventRouter";
 
 import messaging from "./infrastructure/firebase";
+import { Storage } from "@google-cloud/storage";
+
+const keyFilename = require("../config/google-cloud-storage.json");
+const projectId = process.env.PROJECT_ID;
 
 const app = express();
 const dbUri = config.get("MongoDB.dbConfig.host");
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+
+const storage = new Storage({ projectId, keyFilename });
+
+async function uploadFile(bucketName: string, pathString: string) {
+    try {
+        const bucket = storage.bucket(bucketName);
+        return await bucket.upload(pathString);
+    } catch (error) {
+        console.error("Error uploading file:", error);
+    }
+}
 
 const db = new MongoDB(dbUri);
 
@@ -75,6 +90,18 @@ app.post("/api/send-notification", async (req, res) => {
         return res.status(200).json({ success: true, response });
     } catch (error) {
         console.error("Error sending notification:", error);
+        return res.status(500).json({ success: false, error });
+    }
+});
+
+app.post("/api/upload-file", async (req, res) => {
+    const { pathString } = req.body;
+    try {
+        const response = await uploadFile("trainapp-user-profiles", pathString);
+        console.log("File uploaded:", response);
+        return res.status(200).json({ success: true, response });
+    } catch (error) {
+        console.error("Error uploading file:", error);
         return res.status(500).json({ success: false, error });
     }
 });
