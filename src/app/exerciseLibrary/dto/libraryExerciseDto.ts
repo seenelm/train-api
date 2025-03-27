4bb1ac274a41cb7d7b752a9e1e8cb4214e634ead
noahgross1 <noahgross1@gmail.com>
2025-03-27 17:55:53 -0400

    added exercise library

--- src/app/exerciseLibrary/dto/libraryExerciseDto.ts
+++ src/app/exerciseLibrary/dto/libraryExerciseDto.ts
@@ -1,18 +1,35 @@
 import { Types } from "mongoose";
 
+export interface FullLibraryExerciseRequest {
+    libraryExerciseRequest: LibaryExerciseRequest;
+    categoryRequest: CategoryRequest;
+    muscleRequest: MuscleRequest[];
+}
+
 export interface LibaryExerciseRequest {
     name: string;
-    description: string;
-    categoryId: Types.ObjectId;
-    difficulty: string;
+    imagePath?: string;
+    description?: string;
+    categoryId?: Types.ObjectId;
+    difficulty?: string;
     equipment?: string[];
 }
 
 export interface LibaryExerciseResponse {
     id: Types.ObjectId;
     name: string;
-    description: string;
-    categoryId: Types.ObjectId;
-    difficulty: string;
+    imagePath?: string;
+    description?: string;
+    categoryId?: Types.ObjectId;
+    difficulty?: string;
     equipment?: string[];
+}
+
+export interface CategoryRequest {
+    name: string;
+    description?: string;
+}
+
+export interface MuscleRequest {
+    name: string;
 }
