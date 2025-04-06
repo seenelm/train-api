aa62b8947098af7f679406fc452791183a47b8b1
noahgross1 <noahgross1@gmail.com>
2025-04-06 13:42:35 -0400

    added configuration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { app, db } from "./app";
 import mongoose from "mongoose";
+import config from "./common/config";
 
 let server: any = null;
+const port = config.server.port || 3000;
 
 db.connect()
     .then(() => {
         mongoose.set("debug", true);
-        server = app.listen(3000, () => {
-            console.log(`Server is running on port ${3000}`);
+        server = app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
         });
     })
     .catch((error) => {
