4cf3f9a7d1d54c3394cba844218af0bed8e30302
Yassine Elmellouki <yelmellouki@gmail.com>
2025-04-18 14:27:14 -0400

    fixing a couple of things

--- src/infrastructure/firebase.ts
+++ src/infrastructure/firebase.ts
@@ -1,19 +1,23 @@
 import admin from "firebase-admin";
 
-let firebaseConfig: admin.ServiceAccount | undefined;
+const firebaseConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
-// Try to parse the FIREBASE_CONFIG environment variable if it exists
-if (process.env.FIREBASE_CONFIG) {
-  try {
-    firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG) as admin.ServiceAccount;
-  } catch (error) {
-    console.error("Error parsing FIREBASE_CONFIG:", error);
-  }
-} 
 // Initialize Firebase Admin if configuration is available
 if (firebaseConfig) {
   admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig),
+    credential: admin.credential.cert(firebaseConfig as admin.ServiceAccount),
   });
   console.log("Firebase initialized successfully");
 } else {
