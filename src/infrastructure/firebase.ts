c723c25518d4db7703f15d3b28e7cbe0009d7a0d
Yassine Elmellouki <yelmellouki@gmail.com>
2025-04-18 23:22:48 -0400

    working now

--- src/infrastructure/firebase.ts
+++ src/infrastructure/firebase.ts
@@ -1,18 +1,8 @@
 import admin from "firebase-admin";
 
-const firebaseEnvJson = process.env.FIREBASE_CONFIG;
-
-if (!firebaseEnvJson) {
-  console.warn("⚠️ FIREBASE_CONFIG env var is missing.");
-} else {
-  const serviceAccount = JSON.parse(firebaseEnvJson);
-
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-    });
-    console.log("✅ Firebase initialized from env");
-  }
-}
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+console.log("✅ Firebase initialized from env");
 
 export default admin;
